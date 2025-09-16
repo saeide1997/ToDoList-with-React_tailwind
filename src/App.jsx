c9eda124f5f25775a4666b15e6bfd435a5f86c63
saeide1997 <saeiide.tm@gmail.com>
2025-09-16 13:52:15 +0330

    chore: initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-2xl font-bold underline text-amber-300">    Hello world!  </h1>
+    </>
+  )
+}
+
+export default App
